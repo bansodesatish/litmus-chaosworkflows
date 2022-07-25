007603ec1470c47b40f70c24c019d9e29111a63f
satish bansode <bansode.m.satish@gmail.com>
2022-07-25 13:46:30 +0530

    updated chaos script

--- scripts/chaos.sh
+++ scripts/chaos.sh
@@ -15,11 +15,14 @@ cat workflows/new.yaml | sed "s|{{WORKFLOW_ID}}|$BUILD_NUMBER|" | sed "s|{{LITMU
 echo "Deploy workflow from yaml file"
 cat workflows/new.yaml | sed "s|{{WORKFLOW_ID}}|$BUILD_NUMBER|" | sed "s|{{LITMUS_WORKFLOW_AGENT_ID}}|$LITMUS_WORKFLOW_AGENT_ID|" | kubectl apply -f -
 # kubectl apply -f workflows/devoxx-delete.yml
-until kubectl get workflow  --sort-by=.metadata.creationTimestamp -o jsonpath='{.items[-1:].metadata.labels.\workflows\.argoproj\.io\/phase}' -nlitmus | grep -m 1 "Succeeded\|Failed";
-do
-  echo "waiting for the chaos to finish";
-done
 
+# until kubectl get workflow  --sort-by=.metadata.creationTimestamp -o jsonpath='{.items[-1:].metadata.labels.\workflows\.argoproj\.io\/phase}' -nlitmus | grep -m 1 "Succeeded\|Failed";
+set -x
+until kubectl get workflow workflow-$WORKFLOW_ID -o jsonpath='{.metadata.labels.\workflows\.argoproj\.io\/phase}' -nlitmus | grep -m 1 "Succeeded\|Failed";
+do
+  echo "waiting for the \"workflow-$WORKFLOW_ID\" chaos workflow to finish";
+done
+set +x
 sleep 10;
 
 for each in $(kubectl get chaosresult -nlitmus --no-headers -oname);
